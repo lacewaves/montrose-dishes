dbbd48c060a7b63ae82bf9c5abf81a19213b40a5
lacewaves <lacewaves@users.noreply.github.com>
2015-11-05 10:14:15 -0600

    Basic layout & card flipping

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,4 +1,4 @@
 class Dish < ActiveRecord::Base
-  has_many :ingredients
+  has_and_belongs_to_many :ingredients
   belongs_to :restaurant
 end
--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,3 +1,3 @@
 class Ingredient < ActiveRecord::Base
-  has_many :dishes
+  has_and_belongs_to_many :dishes
 end
