0f00e9a79e9676260df60082e94b2c2facdef2cd
lacewaves <lacewaves@users.noreply.github.com>
2015-11-12 10:42:24 -0600

    Trigger card flipping on click, not hover

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,10 +13,3 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
-var map = L.map('map').setView([51.505, -0.09], 1);
-
-L.tileLayer('https://api.tiles.mapbox.com/v4/lacewaves.o51k3fm5/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibGFjZXdhdmVzIiwiYSI6ImNpZ3ZqeGt0dTByYjE0cG0zYjR4cmo5NXoifQ.UGhgkS0p-U6aUrop_2IY4w', {
-    maxZoom: 18,
-}).addTo(map);
-
-var marker = L.marker([51.5, -0.09]).addTo(map);
