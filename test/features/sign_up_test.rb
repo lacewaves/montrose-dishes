f3157e6f3cf65264e015f92f2bd3bdb1f87090d6
lacewaves <lacewaves@users.noreply.github.com>
2016-07-08 13:10:07 -0500

    Add footer

--- test/features/sign_up_test.rb
+++ test/features/sign_up_test.rb
@@ -3,33 +3,19 @@ require "test_helper"
 class SignUpTest < Capybara::Rails::TestCase
   setup do
     nikonikos = Restaurant.create! name: "Niko Niko's", placeId: "ChIJ-SKYT2C_QIYRY_5IDAJOEwM", cuisine: "Greek"
-
     spinachpita = Dish.create! name: "Spinach and Feta Pita", cuisine: "Greek", dish_type: "Sandwich", upvotes: 23, restaurant_id: nikonikos.id
-
     baklava = Dish.create! name: "Baklava", cuisine: "Greek", dish_type: "Pastry", upvotes: 15, restaurant_id: nikonikos.id
-
     spinach = Ingredient.create! name: "Spinach", food_group: "Vegetable"
-
     feta = Ingredient.create! name: "Feta Cheese", food_group: "Dairy"
-
     pita = Ingredient.create! name: "Pita Bread", food_group: "Grains"
-
     honey = Ingredient.create! name: "Honey", food_group: "Sugar"
-
     almonds = Ingredient.create! name: "Almonds", food_group: "Nuts"
-
     pastry = Ingredient.create! name: "Pastry Dough", food_group: "Grains"
-
     DishIngredient.create! dish_id: spinachpita.id, ingredient_id: spinach.id
-
     DishIngredient.create! dish_id: spinachpita.id, ingredient_id: feta.id
-
     DishIngredient.create! dish_id: spinachpita.id, ingredient_id: pita.id
-
     DishIngredient.create! dish_id: baklava.id, ingredient_id: honey.id
-
     DishIngredient.create! dish_id: baklava.id, ingredient_id: almonds.id
-
     DishIngredient.create! dish_id: baklava.id, ingredient_id: pastry.id
   end
 
@@ -43,6 +29,34 @@ class SignUpTest < Capybara::Rails::TestCase
     click_button "Sign In"
 
     assert_content page, "signing in"
+
+end
+
+test "Can Sign Out" do
+    user = User.create! username: "testuser", password: "12345678"
+
+    visit root_path
+    click_link "Sign In"
+    fill_in "Username", with: 'testuser'
+    fill_in "Password", with: "12345678"
+    click_button "Sign In"
+
+    click_link "Sign Out"
+    assert_content "Signed Out!"
+
+end
+
+test "Validates Password" do
+   user = User.create! username: "testuser", password: "12345678"
+   visit root_path
+   click_link "Sign In"
+
+   fill_in "Username", with: 'testuser'
+   fill_in "Password", with: "87654321"
+   click_button "Sign In"
+
+   refute_content page, "signing in"
+   assert_content page, "Something is wrong with your username and/or password."
 
 end
 
